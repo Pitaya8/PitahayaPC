289c389362f6a99efdc837fa4018aff39bee452f
Pitaya <zhuangff@163.com>
2019-04-12 16:00:38 +0800

    完成“联系我们”页面

--- js/login.js
+++ js/login.js
@@ -1,23 +1,44 @@
-let status=1;
+let status = 1;
 $(function () {
   console.log('页面')
- $('#overCurtain').css('height',$(document).height()+'px')
+  $('#overCurtain').css('height', $(document).height() + 'px')
 
 });
+/*
+* 点击登录
+* */
 $("#login").click(function () {
   $(".hide-center").fadeIn("slow");
-  $('#overCurtain').css('display','block')
+  $('#overCurtain').css('display', 'block')
   $(".overCurtain").fadeIn("slow");
 })
+/*
+* 关闭登录窗口
+* */
 $("#close").click(function () {
   $(".hide-center").fadeOut("fast")
-  $(".hide-center").css('display','none')
+  $(".hide-center").css('display', 'none')
   // $(".overCurtain").fadeOut("slow")
-  $('#overCurtain').css('display','none')
+  $('#overCurtain').css('display', 'none')
 })
-if(status){
-  $('#personalInfo').css('display','none');
-}else{
-  $('#personalInfo').css('display','inline-block');
-  $("#login").css('display','none')
+/*
+* 点击退出
+* */
+$('#signout').click(function(){
+  status=0;
+  StatusOptions(status)
+})
+StatusOptions(status);
+function StatusOptions(type){
+  if (type) {
+    $('#personalInfo').css('display', 'inline-block');
+    $("#login").css('display', 'none')
+    $('#register').css('display', 'none')
+  } else {
+    $('#personalInfo').css('display', 'none');
+    $('#signout').css('display', 'none')
+    $("#login").css('display', 'inline-block')
+    $('#register').css('display', 'inline-block')
+  }
 }
+
