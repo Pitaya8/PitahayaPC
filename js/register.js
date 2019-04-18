1cdcf0e185b8950b144a97c225f61455aec50bdd
Pitaya <zhuangff@163.com>
2019-04-18 18:00:01 +0800

    截图二

--- js/register.js
+++ js/register.js
@@ -1,38 +1,3 @@
-//注册页面
-$('#register_phone').blur(function(){
-	if(/\d{11}/g.test(register_phone.value)){
-		// ajax('GET','api/database/insert_user.php','phone='+register_phone.value,
-		// 	function(str){
-		// 	phone_tip.innerText=str;
-		// });
-	}else{
-		phone_tip.innerText='请输入11位数字';
-	}
-})
-
-$('#register_password1').focus(function(){
-	if(register_password.value==''){
-		register_password_tip.innerHTML='请输入密码';
-	}else{
-		if(register_password.value==register_password1.value){
-			register_password1_tip.innerHTML='';
-		}
-		register_password_tip.innerHTML='';
-	}
-},function(){
-	if(register_password.value!=''){
-		if(register_password.value==register_password1.value){
-			register_password1_tip.innerHTML='';
-			register_password_tip.innerHTML='';
-		}else{
-			register_password1_tip.innerHTML='两次密码不一致';
-			register_password_tip.innerHTML='';
-		}
-		
-	}else{
-		register_password_tip.innerHTML='请输入密码';
-	}
-})
 
 //同意注册
 register_btn.onclick=function(){
@@ -53,7 +18,7 @@ register_btn.onclick=function(){
 			// 		},1000)
 			// });
 		}else{
-			register_checkbox_tip.innerHTML='必须同意《用户注册协议》 ！';
+			// register_checkbox_tip.innerHTML='必须同意《用户注册协议》 ！';
 		}
 }
 
